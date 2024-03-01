19745df7227155e2831669d981c3a793bfb32978
bobbyhadz <opmftw@gmail.com>
2024-03-01 16:14:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// EXAMPLE 1 - Get the First and Last Elements of an Array in JavaScript
+
+const arr = ['a', 'b', 'c', 'd'];
+
+const first = arr[0];
+console.log(first); // 👉️ a
+
+const last = arr[arr.length - 1];
+console.log(last); // 👉️ d
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 2 - Get the First and Last Elements of an Array using Array.at()
+
+// const arr = ['a', 'b', 'c', 'd'];
+
+// const first = arr.at(0);
+// console.log(first); // 👉️ a
+
+// const last = arr.at(-1);
+// console.log(last); // 👉️ d
